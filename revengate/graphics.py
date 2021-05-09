06a070423532b658d4c55022e40636becb898936
Yannick Gingras <ygingras@ygingras.net>
2021-05-08 22:45:03 -0600

    proof of concept for the tiles graphical renderer

--- revengate/graphics.py
+++ revengate/graphics.py
@@ -20,6 +20,8 @@
 from pprint import pprint
 import kivy
 from kivy.app import App
+from kivy.core.window import Window
+from kivy.core.text import Label as CoreLabel
 from kivy.uix.widget import Widget
 from kivy.uix.label import Label
 from kivy.uix.button import Button
@@ -40,6 +42,13 @@ IMG_TILE = {TileType.SOLID_ROCK: "dungeon/floor/lair_1_new.png",
 
 
 class MapWidget(Scatter):
+    """ A widget to display a dungeon with graphical tiles. 
+    
+    Two coordinate systems are used:
+    - map: tile id (map.tiles[x][y]), convention is (mx, my)
+    - canvas: pixel position on the canvas, convition is (cy, cy)
+    Both systems have (0, 0) at the bottom-left corner.
+    """
     def __init__(self, map, *args, **kwargs):
         w, h = map.size()
         size = (w*TILE_SIZE, h*TILE_SIZE)
@@ -58,17 +67,45 @@ class MapWidget(Scatter):
         w, h = self.map.size()
 
         with self.canvas:
-            for x in range(w):
+            for mx in range(w):
                 row = []
-                for y in range(h):
-                    pos = (x*TILE_SIZE, y*TILE_SIZE)
-                    t = self._tex[self.map.tiles[x][y]]
+                for my in range(h):
+                    pos = self.map_to_canvas((mx, my))
+                    t = self._tex[self.map.tiles[mx][my]]
                     r = Rectangle(pos=pos, 
                               size=t.size, 
                               texture=t)
                     row.append(r)
                 self.rects.append(row)
-                            
+
+    def map_to_canvas(self, pos):
+        """ Convert an (x, y) tile reference to a pixel position on the canvas.
+        
+        Return the bottom left corner of the rectangle representing the tile.
+        """
+        mx, my = pos
+        return (mx*TILE_SIZE, my*TILE_SIZE)
+        
+    def canvas_to_map(self, pos):
+        """ Convert an (x, y) canvas pixel coordinate into a map tile reference. 
+        """
+        cx, cy = pos
+        return (cx//TILE_SIZE, cy//TILE_SIZE)
+
+
+class Controller(FloatLayout):
+    def __init__(self, *args, **kwargs):
+        super(Controller, self).__init__(*args, **kwargs)
+        self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
+        self._keyboard.bind(on_key_down=self.on_key_down)
+
+    def _keyboard_closed(self):
+        self._keyboard.unbind(on_key_down=self.on_key_down)
+        self._keyboard = None
+    
+    def on_key_down(self, kb, key, scancode, codepoint, modifier, **kwargs):
+        print(f"key_down: {key}")
+
 
 class DemoApp(App):
     def __init__(self, map, *args):
@@ -76,7 +113,7 @@ class DemoApp(App):
         self.map = map
         
     def build(self):
-        cont = FloatLayout()
+        cont = Controller()
         map_wid = MapWidget(self.map, do_rotation=False)
         cont.add_widget(map_wid)
         return cont
