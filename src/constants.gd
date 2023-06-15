41d85590d93e4bb8f8e29dbbf103190ca4b77b92
Yannick Gingras <dev-noreply@ygingras.net>
2023-06-15 16:55:22 +0200

    Bumped version!

--- src/constants.gd
+++ src/constants.gd
@@ -19,7 +19,7 @@
 ## This script is autoloaded at `Consts`
 extends Node
 
-const VERSION := "0.7.2"
+const VERSION := "0.8.0"
 const DEBUG = true
 
 ## The kind of damage, mostly used to compute resistances. Can apply to healing as well.
